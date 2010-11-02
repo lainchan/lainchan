c96313a69a9419e1b63056009e3125eb7dced10f
Savetheinternet <savetheinternet@n0v4.com>
2010-11-02 22:06:37 +1100

    Added SQL dump

---
+++ install.sql
@@ -0,0 +1,54 @@
+-- phpMyAdmin SQL Dump
+-- version 3.3.4
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Nov 02, 2010 at 10:06 PM
+-- Server version: 5.1.48
+-- PHP Version: 5.3.2
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `imgboard`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `posts`
+--
+
+CREATE TABLE IF NOT EXISTS `posts` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `thread` int(11) DEFAULT NULL,
+  `subject` varchar(25) NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `name` varchar(25) NOT NULL,
+  `trip` varchar(15) DEFAULT NULL,
+  `body` text NOT NULL,
+  `time` int(11) NOT NULL,
+  `bump` int(11) DEFAULT NULL,
+  `thumb` varchar(50) DEFAULT NULL,
+  `thumbwidth` int(11) DEFAULT NULL,
+  `thumbheight` int(11) DEFAULT NULL,
+  `file` varchar(50) DEFAULT NULL,
+  `filewidth` int(11) DEFAULT NULL,
+  `fileheight` int(11) DEFAULT NULL,
+  `filesize` int(11) DEFAULT NULL,
+  `filename` varchar(30) DEFAULT NULL,
+  `password` varchar(20) DEFAULT NULL,
+  `ip` varchar(15) NOT NULL,
+  UNIQUE KEY `id` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `posts`
+--
+
