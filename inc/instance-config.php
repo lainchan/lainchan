0296a7666f4e9fa2827edf6cc095fb05ad73440b
Savetheinternet <savetheinternet@n0v4.com>
2010-11-05 01:02:13 +1100

    Comments in configs

---
+++ inc/instance-config.php
@@ -0,0 +1,19 @@
+<?php
+/*
+ *  Instance Configuration
+ *  ----------------------
+ *  Edit this file and not config.php for imageboard configuration.
+ *
+ *  You can copy values from config.php (defaults) and paste them here.
+ */
+	
+	/*
+	
+	define('MY_SERVER',		'localhost', true);
+	define('MY_USER',		'', true);
+	define('MY_PASSWORD',	'', true);
+	define('MY_DATABASE',	'', true);
+	
+//	define('FOO',			'bar');
+	*/
+?>
