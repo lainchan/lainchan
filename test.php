ee9cc2ec243ec55161fba3124f5a3f6c3326c865
Savetheinternet <savetheinternet@n0v4.com>
2010-11-16 00:57:37 +1100

    Bug fix in test.php

--- test.php
+++ test.php
@@ -4,10 +4,10 @@
 	require 'inc/functions.php';
 	require 'inc/display.php';
 	require 'inc/template.php';
-	if (file_exists('inc/instance-instance-config.php')) {
-		require 'inc/instance-instance-config.php';
+	if (file_exists('inc/instance-config.php')) {
+		require 'inc/instance-config.php';
 	}
-	require 'inc/instance-config.php';
+	require 'inc/config.php';
 	require 'inc/user.php';
 	
 	function image($type) {
