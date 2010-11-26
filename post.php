e4683b93e15cc8ad2b8ccf731c796929b4ec2de4
savetheinternet@n0v4.com <savetheinternet@omegadev.org>
2010-11-26 01:20:04 -0800

    Typo in "development"

--- post.php
+++ post.php
@@ -11,13 +11,13 @@
 	$board = Array(
 		'url' => '/b/',
 		'name' => 'Beta',
-		'title' => 'In devleopment.');
+		'title' => 'In development.');
 	
 	$body = '';
 	
-	// Fix for magic quotes
+	 Fix for magic quotes
 	if (get_magic_quotes_gpc()) {
-		function strip_array(&$var) {
+		function strip_array($var) {
 			return is_array($var) ? array_map("strip_array", $var) : stripslashes($var);
 		}
 		
