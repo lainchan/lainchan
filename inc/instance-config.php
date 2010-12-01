3e91dbadd449e4f0e189c2b2dce33d782af31f6f
Savetheinternet <savetheinternet@n0v4.com>
2010-12-01 23:44:57 +1100

    Fixed instance-config.php

--- inc/instance-config.php
+++ inc/instance-config.php
@@ -9,15 +9,15 @@
  */
 	
 	
-	
+	/*
 	// Database stuff
 	define('MY_SERVER',		'127.0.0.1');
-	define('MY_USER',		'imgboard');
-	define('MY_PASSWORD',	'DF3uHz4vvhUM6rCq');
-	define('MY_DATABASE',	'imgboard');
+	define('MY_USER',		'');
+	define('MY_PASSWORD',	'');
+	define('MY_DATABASE',	'');
 	
 	define('ROOT',			'/');
 	
 //	define('FOO',			'bar');
-	
+	*/
 ?>
