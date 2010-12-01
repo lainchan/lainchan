0385f465338ffa3fbcc10056c887b8c236c2533b
Savetheinternet <savetheinternet@n0v4.com>
2010-12-01 21:53:11 +1100

    created mod.php

---
+++ mod.php
@@ -0,0 +1,42 @@
+<?php
+	require 'inc/functions.php';
+	require 'inc/display.php';
+	if (file_exists('inc/instance-config.php')) {
+		require 'inc/instance-config.php';
+	}
+	require 'inc/config.php';
+	require 'inc/template.php';
+	require 'inc/user.php';
+	
+	// If not logged in
+	if(!$user) {
+		if(isset($_POST['login'])) {
+			// Check if inputs are set and not empty
+			if(	!isset($_POST['username']) ||
+				!isset($_POST['password']) ||
+				empty($_POST['username']) ||
+				empty($_POST['password'])
+				) loginForm(ERROR_INVALID, $_POST['username']);
+			
+			// Open connection
+			sql_open();
+			
+			if(!login($_POST['username'], $_POST['password']))
+				loginForm(ERROR_INVALID, $_POST['username']);
+			
+			// Login successful
+			// Set cookies
+			setCookies();
+			
+			// Close connection
+			sql_close();
+		} else {
+			loginForm();
+		}
+	} else {
+		var_dump($_COOKIE);
+		openBoard('b');
+		echo buildThread(1, true);
+	}
+?>
+
