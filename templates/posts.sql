3679f2c78b4ad4c4204738ab0739c9aaad88f751
Savetheinternet <savetheinternet@n0v4.com>
2010-12-02 20:50:14 +1100

    SQL table creation template

---
+++ templates/posts.sql
@@ -0,0 +1,23 @@
+CREATE TABLE IF NOT EXISTS `posts_{board}` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `thread` int(11) DEFAULT NULL,
+  `subject` varchar(40) NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `name` varchar(25) NOT NULL,
+  `trip` varchar(15) DEFAULT NULL,
+  `body` text NOT NULL,
+  `time` int(11) NOT NULL,
+  `bump` int(11) DEFAULT NULL,
+  `thumb` varchar(50) DEFAULT NULL,
+  `thumbwidth` int(11) DEFAULT NULL,
+  `thumbheight` int(11) DEFAULT NULL,
+  `file` varchar(50) DEFAULT NULL,
+  `filewidth` int(11) DEFAULT NULL,
+  `fileheight` int(11) DEFAULT NULL,
+  `filesize` int(11) DEFAULT NULL,
+  `filename` varchar(30) DEFAULT NULL,
+  `filehash` varchar(32) DEFAULT NULL,
+  `password` varchar(20) DEFAULT NULL,
+  `ip` varchar(15) NOT NULL,
+  UNIQUE KEY `id` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
