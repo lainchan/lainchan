5190e9a45698e72eaa58a2138baa370460fc4e13
Savetheinternet <savetheinternet@n0v4.com>
2011-01-02 00:03:12 +1100

    `expires` can be NULL

--- install.sql
+++ install.sql
@@ -109,6 +109,6 @@ INSERT INTO `mods` (`id`, `username`, `password`, `type`) VALUES
 CREATE TABLE IF NOT EXISTS  `bans` (
   `ip` varchar( 15 ) NOT NULL ,
   `mod` int NOT NULL COMMENT  'which mod made the ban',
-  `expires` int NOT NULL,
+  `expires` int NULL,
   `reason` text NULL
 ) ENGINE = InnoDB;
