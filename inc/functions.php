cfc6428219049e34fa0a0bd0830f34bd117d635d
Savetheinternet <savetheinternet@tinyboard.org>
2011-08-18 20:16:02 +1000

    handle post citations with trailing punctuation

--- inc/functions.php
+++ inc/functions.php
@@ -1215,7 +1215,7 @@
 		}
 
 		// Cites
-		if(isset($board) && preg_match_all('/(^|\s)&gt;&gt;(\d+?)(\s|$)/', $body, $cites)) {
+		if(isset($board) && preg_match_all('/(^|\s)&gt;&gt;(\d+?)([\s,.]|$)/', $body, $cites)) {
 			if(count($cites[0]) > $config['max_cites']) {
 				error($config['error']['toomanycites']);
 			}
@@ -1237,7 +1237,7 @@
 		}
 		
 		// Cross-board linking
-		if(preg_match_all('/(^|\s)&gt;&gt;&gt;\/(\w+?)\/(\d+)?(\s|$)/', $body, $cites)) {
+		if(preg_match_all('/(^|\s)&gt;&gt;&gt;\/(\w+?)\/(\d+)?([\s,.]|$)/', $body, $cites)) {
 			if(count($cites[0]) > $config['max_cites']) {
 				error($config['error']['toomanycross']);
 			}
