b2f083be306b604dc213ae35539119de1abc53fb
Savetheinternet <savetheinternet@tinyboard.org>
2011-11-17 08:03:01 +1100

    Fixed bug with some embedded YouTube videos (containing _ or -)

--- inc/config.php
+++ inc/config.php
@@ -510,7 +510,7 @@
 	// It's very important that you match the full string (with ^ and $) or things will not work correctly.
 	$config['embedding'] = Array(
 		Array(
-			'/^https?:\/\/(\w+\.)?youtube\.com\/watch\?v=([a-zA-Z0-9-]{10,11})(&.+)?$/i',
+			'/^https?:\/\/(\w+\.)?youtube\.com\/watch\?v=([a-zA-Z0-9\-_]{10,11})(&.+)?$/i',
 			'<object style="float: left;margin: 10px 20px;" width="%%tb_width%%" height="%%tb_height%%"><param name="movie" value="http://www.youtube.com/v/$2?fs=1&amp;hl=en_US"></param><param name="allowFullScreen" value="true"></param><param name="allowscriptaccess" value="always"></param><embed src="http://www.youtube.com/v/$2?fs=1&amp;hl=en_US" type="application/x-shockwave-flash" width="%%tb_width%%" height="%%tb_height%%" allowscriptaccess="always" allowfullscreen="true"></embed></object>'
 		),
 		Array(
