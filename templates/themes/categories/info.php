5ea4de048a8371597fef2c33b0132084005976b1
Michael Save <savetheinternet@tinyboard.org>
2012-02-14 21:26:08 +1100

    categories theme v0.3

--- templates/themes/categories/info.php
+++ templates/themes/categories/info.php
@@ -7,14 +7,14 @@
 	$theme['description'] = 
 'Group-ordered, category-aware modification of the Frameset theme, with removable sidebar frame.
 
-Requires $config[\'boards\'] and $config[\'categories\'].';
-	$theme['version'] = 'v0.2.1';
+Requires $config[\'categories\'].';
+	$theme['version'] = 'v0.2.2';
 	
 	// Theme configuration	
 	$theme['config'] = Array();
 	
 	$theme['config'][] = Array(
-		'title' => 'Title',
+		'title' => 'Site title',
 		'name' => 'title',
 		'type' => 'text'
 	);
@@ -22,18 +22,43 @@ Requires $config[\'boards\'] and $config[\'categories\'].';
 	$theme['config'][] = Array(
 		'title' => 'Slogan',
 		'name' => 'subtitle',
-		'type' => 'text'
+		'type' => 'text',
+		'comment' => '(optional)'
+	);
+	
+	$theme['config'][] = Array(
+		'title' => 'Main HTML file',
+		'name' => 'file_main',
+		'type' => 'text',
+		'default' => $config['file_index'],
+		'comment' => '(eg. "index.html")'
+	);
+	
+	$theme['config'][] = Array(
+		'title' => 'Sidebar file',
+		'name' => 'file_sidebar',
+		'type' => 'text',
+		'default' => 'sidebar.html',
+		'comment' => '(eg. "sidebar.html")'
+	);
+	
+	$theme['config'][] = Array(
+		'title' => 'News file',
+		'name' => 'file_news',
+		'type' => 'text',
+		'default' => 'news.html',
+		'comment' => '(eg. "news.html")'
 	);
 	
 	// Unique function name for building everything
 	$theme['build_function'] = 'categories_build';
-	
 	$theme['install_callback'] = 'categories_install';
+	
 	if(!function_exists('categories_install')) {
 		function categories_install($settings) {
 			global $config;
 			
-			if(!isset($config['boards']) || !isset($config['categories'])) {
+			if(!isset($config['categories'])) {
 				return Array(false, '<h2>Prerequisites not met!</h2>' . 
 					'This theme requires $config[\'boards\'] and $config[\'categories\'] to be set.');
 			}
--- templates/themes/categories/theme.php
+++ templates/themes/categories/theme.php
@@ -16,142 +16,56 @@
 			global $config;
 			
 			if($action == 'all')
-				file_write($config['dir']['home'] . $config['file_index'], Categories::homepage($settings));
+				file_write($config['dir']['home'] . $settings['file_main'], Categories::homepage($settings));
 			
 			if($action == 'all' || $action == 'boards')
-				file_write($config['dir']['home'] . 'sidebar.html', Categories::sidebar($settings));
+				file_write($config['dir']['home'] . $settings['file_sidebar'], Categories::sidebar($settings));
 			
 			if($action == 'all' || $action == 'news')
-				file_write($config['dir']['home'] . 'news.html', Categories::news($settings));
+				file_write($config['dir']['home'] . $settings['file_news'], Categories::news($settings));
 		}
 		
 		// Build homepage
 		public static function homepage($settings) {
 			global $config;
 			
-			// HTML5
-			return '<!DOCTYPE html><html>'
-			. '<head>'
-				. '<link rel="stylesheet" media="screen" href="' . $config['url_stylesheet'] . '"/>'
-				. '<style type="text/css">'
-					. 'iframe{border:none;margin:0;padding:0;height:99%;position:absolute}'
-					. 'iframe#sidebar{left:0;top:0;width:15%}'
-					. 'iframe#main{border-left:1px solid black;left:15%;top:0;width:85%}'
-				. '</style>'
-				. '<title>' . $settings['title'] . '</title>'
-				. '<script type="text/javascript">'
-					. 'function removeFrames() {'
-					. 'window.location = document.getElementById("main").contentWindow.location.href'
-					. '}'
-				. '</script>'
-			. '</head><body>'
-			// Sidebar
-			. '<iframe src="sidebar.html" id="sidebar" name="sidebar"></iframe>'
-			// Main
-			. '<iframe src="news.html" id="main" name="main"></iframe>'
-			// Finish page
-			. '</body></html>';
+			return Element('themes/frameset/frames.html', Array('config' => $config, 'settings' => $settings));
 		}
 		
 		// Build news page
 		public static function news($settings) {
 			global $config;
 			
-			// HTML5
-			$body = '<!DOCTYPE html><html>'
-			. '<head>'
-				. '<link rel="stylesheet" media="screen" href="' . $config['url_stylesheet'] . '"/>'
-				. '<title>News</title>'
-			. '</head><body>';
-			
-			$boardlist = createBoardlist();
-			
-			$body .= $boardlist['top']
-				
-			. '<h1>' . $settings['title'] . '</h1>'
-				. '<div class="title">' . ($settings['subtitle'] ? utf8tohtml($settings['subtitle']) : '') . '</div>';
-			
 			$query = query("SELECT * FROM `news` ORDER BY `time` DESC") or error(db_error());
-			if($query->rowCount() == 0) {
-				$body .= '<p style="text-align:center" class="unimportant">(No news to show.)</p>';
-			} else {
-				// List news
-				while($news = $query->fetch()) {
-					$body .= '<div class="ban">' .
-					'<h2 id="' . $news['id'] . '">' .
-						($news['subject'] ?
-							$news['subject']
-						:
-							'<em>no subject</em>'
-						) .
-					'<span class="unimportant"> &mdash; by ' .
-						$news['name'] .
-					' at ' .
-						strftime($config['post_date'], $news['time']) .
-					'</span></h2><p>' . $news['body'] . '</p></div>';
-				}
-			}
+			$news = $query->fetchAll(PDO::FETCH_ASSOC);
 			
-			// Finish page
-			$body .= '</body></html>';
-			
-			return $body;
+			return Element('themes/frameset/news.html', Array(
+				'settings' => $settings,
+				'config' => $config,
+				'news' => $news
+			));
 		}
 		
 		// Build sidebar
 		public static function sidebar($settings) {
 			global $config, $board;
 			
-			$body = '<!DOCTYPE html><html>'
-			. '<head>' 
-				. '<link rel="stylesheet" media="screen" href="' . $config['url_stylesheet'] . '"/>'
-				. '<style type="text/css">'
-					. 'fieldset{margin:10px 0}'
-					. 'legend{width:100%;margin-left:-15px;background:#98E;border:1px solid white;color:white;font-weight:bold;padding:5px 5px}'
-					. 'ul{margin:0;padding:0}'
-					. 'li{list-style:none;padding:0 4px;margin: 0 4px}'
-					. 'li a.system{font-weight:bold}'
-				. '</style>'
-				. '<base target="main" />'
-				. '<title>' . $settings['title'] . '</title>'
-			. '</head><body>';
+			$categories = $config['categories'];
 			
-			$body .= '<fieldset><legend>' . $settings['title'] . '</legend><ul>' .
-				'<li><a class="system" href="news.html">[News]</a></li>' .
-				'<li><a class="system" href="javascript:parent.removeFrames()">[Remove Frames]</a></li>' .
-			'</ul></fieldset>';
-			
-			if(isset($config['categories'])) {
-				for($cat = 0; $cat < count($config['categories']); $cat++) {
-					$body .= '<fieldset><legend>' . $config['categories'][$cat] . '</legend><ul>';
-				
-					foreach($config['boards'][$cat] as &$board) {
-						$body .= '<li><a href="' .
-							sprintf($config['board_path'], $board) .
-						'">' . boardTitle($board) . '</a></li>';
-					}
-				
-					$body .= '</ul></fieldset>';
+			foreach($categories as &$boards) {
+				foreach($boards as &$board) {
+					$title = boardTitle($board);
+					if(!$title)
+						$title = $board; // board doesn't exist, but for some reason you want to display it anyway
+					$board = Array('title' => $title, 'uri' => sprintf($config['board_path'], $board));
 				}
 			}
 			
-			if(isset($config['custom_categories'])) {
-				foreach($config['custom_categories'] as $name => &$group) {
-					$body .= '<fieldset><legend>' . $name . '</legend><ul>';
-				
-					foreach($group as $title => &$url) {
-						$body .= '<li><a href="' . $url .
-						'">' . $title . '</a></li>';
-					}
-				
-					$body .= '</ul></fieldset>';
-				}
-			}
-			
-			// Finish page
-			$body .= '</body></html>';
-			
-			return $body;
+			return Element('themes/frameset/sidebar.html', Array(
+				'settings' => $settings,
+				'config' => $config,
+				'categories' => $categories
+			));
 		}
 	};
 	
