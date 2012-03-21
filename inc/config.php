a77b0b5f706a30052168a588a0e4a91605a7d7f3
Michael Save <savetheinternet@tinyboard.org>
2012-03-21 12:11:53 +1100

    PHP syntax highlighting is supposed to be an example. Comment it out.

--- inc/config.php
+++ inc/config.php
@@ -343,12 +343,12 @@
 	$config['markup'][] = Array("/(^|\n)==(.+?)==\n?/m", "<span class=\"heading\">\$2</span>");
 	
 	// Highlight PHP code wrapped in <code> tags (PHP 5.3.0+)
-	$config['markup'][] = Array(
-		'/^&lt;code&gt;(.+)&lt;\/code&gt;/s',
-		function($matches) {
-			return '<code>' . highlight_string(html_entity_decode($matches[1]), true) . '</code>';
-		}
-	);
+	// $config['markup'][] = Array(
+	// 	'/^&lt;code&gt;(.+)&lt;\/code&gt;/s',
+	// 	function($matches) {
+	// 		return '<code>' . highlight_string(html_entity_decode($matches[1]), true) . '</code>';
+	// 	}
+	// );
 	
 /*
  * ====================
