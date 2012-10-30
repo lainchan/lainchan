b1c1a25c26ceac45793536c9a4fe903075cc3b63
Macil Tech <maciltech@gmail.com>
2012-10-29 22:10:58 -0600

    Add (`thread`, `id`) multi-column key to optimize rebuilding index pages.
    This SQL command can be used to add the key to existing board tables:
    create index `thread_id` on posts_%s (`thread`, `id`);

--- templates/posts.sql
+++ templates/posts.sql
@@ -27,6 +27,7 @@ CREATE TABLE IF NOT EXISTS `posts_{{ board }}` (
   `embed` text,
   UNIQUE KEY `id` (`id`),
   KEY `thread` (`thread`),
+  KEY `thread_id` (`thread`, `id`),
   KEY `time` (`time`),
   FULLTEXT KEY `body` (`body`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
