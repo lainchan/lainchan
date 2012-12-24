c1a737d4b4660f6c1c490b4e7099b0d90d682bf0
Marcin Łabanowski <marcin@6irc.net>
2012-12-24 03:54:57 +0100

    attention bar: optimize

--- js/attention-bar.js
+++ js/attention-bar.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
  $("#attention_bar").click(function(eO){ $("#attention_bar").css("display","none");
 	 $("#attention_bar_form").css("display","block"); });
- $.get("/attentionbar.php", function(data) {
+ $.get("/attentionbar.txt", function(data) {
   $("#attention_bar").text(data);
   $("#attention_bar_input").val(data);
  });
