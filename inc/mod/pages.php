00bd5e8d2033c14fe1fd7b15a0db1b1827d549b9
Macil Tech <maciltech@gmail.com>
2013-01-18 23:13:08 -0600

    Remove buildThread() call from mod_deletefile because deleteFile() already calls it.

--- inc/mod/pages.php
+++ inc/mod/pages.php
@@ -1019,13 +1019,6 @@ function mod_deletefile($board, $post) {
 	// Record the action
 	modLog("Deleted file from post #{$post}");
 	
-	$query = prepare(sprintf('SELECT `thread` FROM `posts_%s` WHERE `id` = :id', $board));
-	$query->bindValue(':id', $post);
-	$query->execute() or error(db_error($query));
-	$thread = $query->fetchColumn();
-	
-	// Rebuild thread
-	buildThread($thread ? $thread : $post);
 	// Rebuild board
 	buildIndex();
 	
