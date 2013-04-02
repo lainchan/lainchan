a92bb182d91ae98b2a2bc7787d5650050868f1f1
Macil Tech <maciltech@gmail.com>
2013-04-02 17:28:04 +0800

    Fix display issues with RTL control characters in post names, subjects, and filenames.

--- inc/lib/Twig/Extensions/Extension/Tinyboard.php
+++ inc/lib/Twig/Extensions/Extension/Tinyboard.php
@@ -25,6 +25,7 @@ class Twig_Extensions_Extension_Tinyboard extends Twig_Extension
 			'until' => new Twig_Filter_Function('until'),
 			'split' => new Twig_Filter_Function('twig_split_filter'),
 			'push' => new Twig_Filter_Function('twig_push_filter'),
+			'bidi_cleanup' => new Twig_Filter_Function('bidi_cleanup'),
 			'addslashes' => new Twig_Filter_Function('addslashes')
 		);
 	}
