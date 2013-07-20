bdffdcb1c319cab6f3fec638a17c933d9f4a42ad
czaks <marcin@6irc.net>
2013-07-20 18:11:25 -0400

    youtube.js: made it actually work, also a fix in usage

--- js/youtube.js
+++ js/youtube.js
@@ -13,8 +13,8 @@
 *
 * Usage:
 *	$config['embedding'] = array();
-*	$config['embedding'][] = array(
-*		'/^https?:\/\/(\w+\.)?youtube\.com\/watch\?v=([a-zA-Z0-9\-_]{10,11})(&.+)?$/i',
+*	$config['embedding'][0] = array(
+*		'/^https?:\/\/(\w+\.)?(?:youtube\.com\/watch\?v=|youtu\.be\/)([a-zA-Z0-9\-_]{10,11})(&.+)?$/i',
 *		'<div class="video-container" data-video="$2"><a href="$0" target="_blank" class="file"><img style="width:360px;height:270px;" src="http://img.youtube.com/vi/$2/0.jpg"/></a></div>'
 );
 *   $config['additional_javascript'][] = 'js/jquery.min.js';
@@ -25,9 +25,9 @@
 
 onready(function(){
 	$('div.video-container a').click(function() {
-		var videoID = $(this).parentNode.data('video');
+		var videoID = $(this.parentNode).data('video');
 		
-		$(this).html('<iframe style="float:left;margin: 10px 20px" type="text/html" width="360" height="270" src="http://www.youtube.com/embed/' + videoID + '?autoplay=1" frameborder="0"/>');
+		$(this.parentNode).html('<iframe style="float:left;margin: 10px 20px" type="text/html" width="360" height="270" src="http://www.youtube.com/embed/' + videoID + '?autoplay=1" frameborder="0"/>');
 
 		return false;
 	});
