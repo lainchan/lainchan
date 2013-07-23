1132a4ce7919bbeba1bd98836f7e7316fe7830e9
Michael Foster <savetheinternet@tinyboard.org>
2013-07-23 09:38:42 -0400

    Option to use font-awesome for sticky/lock icons, etc.

--- inc/config.php
+++ inc/config.php
@@ -563,6 +563,12 @@
 	// Make stylesheet selections board-specific
 	$config['stylesheets_board'] = false;
 	
+	// Use Font-Awesome for displaying lock and pin icons, instead of the images in static/.
+	// http://fortawesome.github.io/Font-Awesome/icon/pushpin/
+	// http://fortawesome.github.io/Font-Awesome/icon/lock/
+	$config['font_awesome'] = false;
+	$config['font_awesome_css'] = 'stylesheets/font-awesome/css/font-awesome.min.css';
+	
 	// Boardlinks
 	// You can group, order and place the boardlist at the top of every page, using the following template.	
 	//$config['boards'] = array(
