1157864612957cbb23a19655315abab9b197e203
czaks <marcin@6irc.net>
2013-07-27 01:19:30 -0400

    banned.php: remove polish strings; html5; needs further work

--- banned.php
+++ banned.php
@@ -1,7 +1,7 @@
 <?php
  require_once 'inc/functions.php';
  checkBan();
- print "<html><head>Banned?</head><body>";
- print "<h1>Nie jestes zbanowany.</h1>";
+ print "<!doctype html><html><head><meta charset='utf-8'><title>"._("Banned?")."</title></head><body>";
+ print "<h1>"._("You are not banned.")."</h1>";
  print "</body></html>";
 ?>
