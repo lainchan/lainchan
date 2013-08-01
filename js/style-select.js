cf0a20a5645e1e82a48ad00f9381813af3b4e658
Michael Foster <savetheinternet@tinyboard.org>
2013-08-01 19:21:15 -0400

    js/style-select.js: Changes the stylesheet chooser links to a <select>

---
+++ js/style-select.js
@@ -0,0 +1,43 @@
+/*
+ * style-select.js
+ * https://github.com/savetheinternet/Tinyboard/blob/master/js/style-select.js
+ *
+ * Changes the stylesheet chooser links to a <select>
+ *
+ * Released under the MIT license
+ * Copyright (c) 2013 Michael Save <savetheinternet@tinyboard.org>
+ *
+ * Usage:
+ *   $config['additional_javascript'][] = 'js/jquery.min.js';
+ *   $config['additional_javascript'][] = 'js/style-select.js';
+ *
+ */
+
+onready(function(){
+	var stylesDiv = $('div.styles');
+	var stylesSelect = $('<select></select>');
+	
+	var i = 1;
+	stylesDiv.children().each(function() {
+		stylesSelect.append(
+			$('<option></option>')
+				.text(this.innerText.replace(/(^\[|\]$)/g, ''))
+				.val(i)
+		);
+		$(this).attr('id', 'style-select-' + i);
+		i++;
+	});
+	
+	stylesSelect.change(function() {
+		$('#style-select-' + $(this).val()).click();
+	});
+	
+	stylesDiv.hide();
+	
+	stylesDiv.after(
+		$('<div style="float:right;margin-bottom:10px"></div>')
+			.text(_('Style: '))
+			.append(stylesSelect)
+	);
+});
+
