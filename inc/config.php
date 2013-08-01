235ee484fd1b2f665da2b1402d7393033056a3f7
czaks <marcin@6irc.net>
2013-08-01 01:04:09 -0400

    removed a relict from older post hider

--- inc/config.php
+++ inc/config.php
@@ -636,9 +636,6 @@
 	// $config['additional_javascript'][] = 'js/jquery.min.js';
 	// $config['additional_javascript'][] = 'js/auto-reload.js';
 
-	// Enable hiding posts. Remember to put this AFTER jQuery.
-	// $config['additional_javascript'][] = 'js/post-hider.js';
-	 
 	// Where these script files are located on the web (defaults to $config['root']).
 	// $config['additional_javascript_url'] = '/js/';
 	
