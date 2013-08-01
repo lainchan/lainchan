055c3a73820dd949ab8bf71012fac66cac9daebd
czaks <marcin@6irc.net>
2013-07-31 21:22:32 -0400

    Revert "facilitate using markup modifiers multiple times; make raw html not consume the whole post"
    This reverts commit e7dd0bbc4a4a17a4f7e5f6750b48ee3c0946c40e.

--- inc/functions.php
+++ inc/functions.php
@@ -1449,7 +1449,7 @@ function markup(&$body, $track_cites = false) {
 	$body = str_replace("\r", '', $body);
 	$body = utf8tohtml($body);
 	
-	if (preg_match_all('@&lt;tinyboard ([\w\s]+)&gt;(.+?)&lt;/tinyboard&gt;@um', $body, $modifiers, PREG_SET_ORDER | PREG_OFFSET_CAPTURE)) {
+	if (preg_match_all('@&lt;tinyboard ([\w\s]+)&gt;(.+)&lt;/tinyboard&gt;@um', $body, $modifiers, PREG_SET_ORDER | PREG_OFFSET_CAPTURE)) {
 		$skip_chars = 0;
 		$body_tmp = $body;
 		
@@ -1466,7 +1466,8 @@ function markup(&$body, $track_cites = false) {
 				// Public ban message
 				$replacement = sprintf($config['mod']['ban_message'], $modifier['content']);
 			} elseif ($modifier['type'] == 'raw html') {
-				$replacement = html_entity_decode($modifier['content']);
+				$body = html_entity_decode($modifier['content']);
+				return array();
 			} elseif (preg_match('/^escape /', $modifier['type'])) {
 				// Escaped (not a real modifier)
 				$replacement = '&lt;tinyboard ' . substr($modifier['type'], strlen('escape ')) . '&gt;' . $modifier['content'] . '&lt;/tinyboard&gt;';
