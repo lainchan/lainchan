ccfc1501afd678f1e7e5e1c853f1ad2584e90a36
czaks <marcin@6irc.net>
2013-08-18 22:11:41 -0400

    tag for vichan-devel-4.0.11-gold

--- install.php
+++ install.php
@@ -1,7 +1,7 @@
 <?php
 
 // Installation/upgrade file	
-define('VERSION', 'v0.9.6-dev-12 + <a href="https://int.vichan.net/devel/">vichan-devel-4.0.10</a>');
+define('VERSION', 'v0.9.6-dev-12 + <a href="https://int.vichan.net/devel/">vichan-devel-4.0.11-gold</a>');
 
 require 'inc/functions.php';
 
@@ -376,6 +376,7 @@ if (file_exists($config['has_installed'])) {
 					$board['uri'])) or error(db_error());
 			}
 		case 'v0.9.6-dev-12':
+		case 'v0.9.6-dev-12 + <a href="https://int.vichan.net/devel/">vichan-devel-4.0.10</a>':
 		case false:
 			// Update version number
 			file_write($config['has_installed'], VERSION);
