198e68d3995acf6b63ebd6f6d99a2619c9aa880d
czaks <marcin@6irc.net>
2013-12-24 16:41:11 +0100

    remove rrdtool theme; STI told me that it never really worked

--- templates/themes/rrdtool/cron.php
+++
@@ -1,9 +0,0 @@
-<?php
-	$_SERVER = Array('REQUEST_URI' => '', 'HTTP_HOST' => '', 'SCRIPT_FILENAME' => '');
-	chdir(str_replace('\\', '/', dirname(__FILE__)) . '/../../../');
-	
-	require 'inc/functions.php';
-	
-	require dirname(__FILE__) . '/theme.php';
-	rebuildTheme('rrdtool', 'cron');
-
