8781d5ee67339383064f4fa4653a027ef47a755e
czaks <marcin@6irc.net>
2013-12-27 15:35:14 +0100

    toggle-locked-threads.js: fix outstanding bug and make it work with ukko

--- js/toggle-locked-threads.js
+++ js/toggle-locked-threads.js
@@ -56,9 +56,16 @@ $(document).ready(function(){
 		});
 	
 	if (hide_locked_threads) {
-		$('img.icon[title="Locked"]').each(function() {
+		$('img.icon[title="Locked"], i.icon-lock.icon').each(function() {
 			hideLockedThread(getThreadFromIcon($(this)));
 		});
 	}
+        $(document).bind('new_post', function(e, post) {
+		if (hide_locked_threads) {
+			$(post).find('img.icon[title="Locked"], i.icon-lock.icon').each(function() {
+	                        hideLockedThread(getThreadFromIcon($(this)));
+       		        });
+		}
+	});
 });
 
