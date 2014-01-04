9a5f3b2e31a00d13a8ae5320720d8a26976debc0
czaks <marcin@6irc.net>
2014-01-04 19:09:30 +0100

    image resizing: fix a problem with some pngs; reported by urban

--- inc/image.php
+++ inc/image.php
@@ -338,8 +338,10 @@ class ImageConvert extends ImageBase {
 						$this->width,
 						$this->height,
 						escapeshellarg($this->temp)))) || !file_exists($this->temp)) {
-					$this->destroy();
-					error('Failed to resize image!', null, $error);
+					if (!preg_match ('/sBIT: invalid/', $error)) {
+						$this->destroy();
+						error('Failed to resize image!', null, $error);
+					}
 				}
 				if ($size = $this->get_size($this->temp)) {
 					$this->width = $size[0];
@@ -361,8 +363,10 @@ class ImageConvert extends ImageBase {
 					$this->width,
 					$this->height,
 					escapeshellarg($this->temp)))) || !file_exists($this->temp)) {
-				$this->destroy();
-				error('Failed to resize image!', null, $error);
+                                if (!preg_match ('/sBIT: invalid/', $error)) {
+					$this->destroy();
+					error('Failed to resize image!', null, $error);
+				}
 			}
 			if ($size = $this->get_size($this->temp)) {
 				$this->width = $size[0];
