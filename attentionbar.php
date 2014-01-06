c8bccecc2327316626a12729806f7018d3d3d3a2
czaks <marcin@6irc.net>
2014-01-06 04:14:51 +0100

    attention bar: iteration of fixes

--- attentionbar.php
+++ attentionbar.php
@@ -1,8 +1,9 @@
 <?php
  require_once 'inc/functions.php';
  checkBan();
- if(@strlen($_POST["text"])>0) {
-	file_put_contents("attentionbar.txt",$_POST["text"]);
+ $text = isset($_POST['text']) ? $_POST['text'] : '';
+ if(strlen($text)>0 && !preg_match('/a href/', $text)) {
+	file_put_contents("attentionbar.txt",$text);
 	 if(strlen($_SERVER['HTTP_REFERER'])>0) { header('Location: ' . $_SERVER['HTTP_REFERER']); }
  	else { header('Location: /'); }
  } else print(file_get_contents("attentionbar.txt"));
