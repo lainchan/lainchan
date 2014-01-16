5cef2d587072108bf006035745b76a6f4e00af62
czaks <marcin@6irc.net>
2014-01-17 00:42:49 +0100

    toggle-locked-threads.js: update to font-awesome-3.0

--- js/toggle-locked-threads.js
+++ js/toggle-locked-threads.js
@@ -41,12 +41,12 @@ $(document).ready(function(){
 		.click(function() {
 			hide_locked_threads = !hide_locked_threads;
 			if (hide_locked_threads) {
-				$('img.icon[title="Locked"], i.icon-lock.icon').each(function() {
+				$('img.icon[title="Locked"], i.fa-lock.fa').each(function() {
 					hideLockedThread(getThreadFromIcon($(this)));
 				});
 				localStorage.hidelockedthreads = true;
 			} else {
-				$('img.icon[title="Locked"], i.icon-lock.icon').each(function() {
+				$('img.icon[title="Locked"], i.fa-lock.fa').each(function() {
 					restoreLockedThread(getThreadFromIcon($(this)));
 				});
 				delete localStorage.hidelockedthreads;
@@ -56,13 +56,13 @@ $(document).ready(function(){
 		});
 	
 	if (hide_locked_threads) {
-		$('img.icon[title="Locked"], i.icon-lock.icon').each(function() {
+		$('img.icon[title="Locked"], i.fa-lock.fa').each(function() {
 			hideLockedThread(getThreadFromIcon($(this)));
 		});
 	}
         $(document).bind('new_post', function(e, post) {
 		if (hide_locked_threads) {
-			$(post).find('img.icon[title="Locked"], i.icon-lock.icon').each(function() {
+			$(post).find('img.icon[title="Locked"], i.fa-lock.fa').each(function() {
 	                        hideLockedThread(getThreadFromIcon($(this)));
        		        });
 		}
