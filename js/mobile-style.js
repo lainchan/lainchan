875a2df5215b0e7e482ea3c68184765d81627845
czaks <marcin@6irc.net>
2014-01-19 13:42:41 +0100

    document mobile-style.js ( vichan-devel/Tinyboard#26 )

--- js/mobile-style.js
+++ js/mobile-style.js
@@ -1,3 +1,16 @@
+/*
+ * mobile-style.js - adds some responsiveness to Tinyboard
+ * https://github.com/vichan-devel/Tinyboard/blob/master/js/mobile-style.js
+ *
+ * Released under the MIT license
+ * Copyright (c) 2014 Marcin Łabanowski <marcin@6irc.net>
+ *
+ * Usage:
+ *   $config['api']['enabled'] = true;
+ *   $config['additional_javascript'][] = 'js/jquery.min.js';
+ *   $config['additional_javascript'][] = 'js/mobile-style.js';
+ */
+
 if(navigator.userAgent.match(/iPhone|iPod|iPad|Android|Opera Mini|Blackberry|PlayBook|Windows Phone|Tablet PC|Windows CE|IEMobile/i)) {
 	$('html').addClass("mobile-style");
 	device_type = "mobile";
