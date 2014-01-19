666e5e5a5fd94c6aeca38e5ce8564b8328cb556a
czaks <marcin@6irc.net>
2014-01-19 13:54:14 +0100

    remove chrome-board-expanding-fix.js: we don't support nested boards in boardlist anyway at the current time

--- js/chrome-board-expanding-fix.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-  $(".desktop-style .sub .sub").on("mouseover", function() {
-    $(this).addClass("hover");
-  }).on("mouseout", function() {
-    $(this).removeClass("hover");
-  });
-});
