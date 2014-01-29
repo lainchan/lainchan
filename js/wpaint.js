764d15a7b65c692f538831bf8da092d79e604956
czaks <marcin@6irc.net>
2014-01-29 21:30:48 +0100

    wpaint.js: start the plugin with documentation ( vichan-devel#20 )

---
+++ js/wpaint.js
@@ -0,0 +1,21 @@
+/*
+ * wpaint.js - wPaint integration javascript
+ * https://github.com/vichan-devel/Tinyboard/blob/master/js/wpaint.js
+ *
+ * Released under the MIT license
+ * Copyright (c) 2014 Marcin Łabanowski <marcin@6irc.net>
+ *
+ * Usage:
+ *   $config['additional_javascript'][] = 'js/jquery.min.js';
+ *   $config['additional_javascript'][] = 'js/jquery-ui.custom.min.js';
+ *   $config['additional_javascript'][] = 'js/ajax.js';
+ *   $config['additional_javascript'][] = 'js/wPaint/lib/wColorPicker.min.js';
+ *   $config['additional_javascript'][] = 'js/wPaint/lib/wPaint.min.js';
+ *   $config['additional_javascript'][] = 'js/wPaint/lib/plugins/main/wPaint.menu.main.min.js';
+ *   //$config['additional_javascript'][] = 'js/wPaint/lib/plugins/text/wPaint.menu.text.min.js';
+ *   //$config['additional_javascript'][] = 'js/wPaint/lib/plugins/shapes/wPaint.menu.main.shapes.min.js';
+ *   //$config['additional_javascript'][] = 'js/wPaint/lib/plugins/file/wPaint.menu.main.file.min.js';
+ *   $config['additional_javascript'][] = 'js/wpaint.js';
+ *
+ */
+
