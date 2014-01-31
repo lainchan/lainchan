3a97ff1a7e49354c40dacb26a94b1f574ecfbda7
czaks <marcin@6irc.net>
2014-01-31 18:12:08 +0100

    wpaint.js: prevent submission on color change

--- js/wpaint.js
+++ js/wpaint.js
@@ -86,6 +86,8 @@ oekaki.init = function() {
         alsoResize: "#wpaintdiv, #wpaintdiv canvas",
       });
 
+
+      $("#wpaintdiv input").on("keydown", function(e) { if (e.keycode == 13) e.preventDefault(); });
       $('#wpaintctr .ui-resizable-se').css({'height':'12px', 'width':'12px'});
     }
   });
