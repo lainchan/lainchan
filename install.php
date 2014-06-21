f4fd8162024b8f81c22077d30dd98a5af2ab51fd
czaks <marcin@6irc.net>
2014-06-21 21:14:51 +0200

    tag for 4.5.1

--- install.php
+++ install.php
@@ -1,7 +1,7 @@
 <?php
 
 // Installation/upgrade file	
-define('VERSION', '4.5.0');
+define('VERSION', '4.5.1');
 
 require 'inc/functions.php';
 
@@ -522,6 +522,7 @@ if (file_exists($config['has_installed'])) {
 			if (!$twig) load_twig();
 			$twig->clearCacheFiles();
 		case '4.4.98':
+		case '4.5.0':
 		case false:
 			// TODO: enhance Tinyboard -> vichan upgrade path.
 			query("CREATE TABLE IF NOT EXISTS ``search_queries`` (  `ip` varchar(39) NOT NULL,  `time` int(11) NOT NULL,  `query` text NOT NULL) ENGINE=MyISAM DEFAULT CHARSET=utf8;") or error(db_error());
