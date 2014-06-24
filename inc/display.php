9df6ca8dddb8dabfa30069cdf5f1e0bb45cea793
8chan <admin@8chan.co>
2014-06-24 19:40:16 +0000

    Fix issue #45

--- inc/display.php
+++ inc/display.php
@@ -380,10 +380,10 @@ class Post {
 				$this->body
 			);
 	}
-	public function link($pre = '') {
+	public function link($pre = '', $page = false) {
 		global $config, $board;
 		
-		return $this->root . $board['dir'] . $config['dir']['res'] . sprintf($config['file_page'], $this->thread) . '#' . $pre . $this->id;
+		return $this->root . $board['dir'] . $config['dir']['res'] . sprintf(($page ? $page : $config['file_page']), $this->thread) . '#' . $pre . $this->id;
 	}
 	
 	public function build($index=false) {
@@ -435,10 +435,10 @@ class Thread {
 				$this->body
 			);
 	}
-	public function link($pre = '') {
+	public function link($pre = '', $page = false) {
 		global $config, $board;
 		
-		return $this->root . $board['dir'] . $config['dir']['res'] . sprintf($config['file_page'], $this->id) . '#' . $pre . $this->id;
+		return $this->root . $board['dir'] . $config['dir']['res'] . sprintf(($page ? $page : $config['file_page']), $this->id) . '#' . $pre . $this->id;
 	}
 	public function add(Post $post) {
 		$this->posts[] = $post;
