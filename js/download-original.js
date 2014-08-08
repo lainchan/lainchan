96f5770e2e216056a62b239b5df6a2acc7fc9531
czaks <marcin@6irc.net>
2014-08-08 21:56:15 +0200

    leave the download-original js for compatibility

---
+++ js/download-original.js
@@ -0,0 +1,5 @@
+/* Remove this file from your inc/instance-config.php
+
+   Consult this: https://github.com/dubcheck/Alokal/commit/a7c710ebcd39fef1430aa4cab2508a9e5ea60fcf
+
+   This file is preserved here for compatibility; will be removed on the next major version */
