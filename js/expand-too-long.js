1b2f5123392be52631b793cbd1533a92c799fc8a
czaks <marcin@6irc.net>
2014-08-09 04:56:50 +0200

    final solution to expand-too-long problems

--- js/expand-too-long.js
+++ js/expand-too-long.js
@@ -23,7 +23,7 @@ $(function() {
 				url: url,
 				context: document.body,
 				success: function(data) {
-					var content = $(data).find('#'+url.split('#')[1]).parent().find(".body").html();
+					var content = $(data).find('#'+url.split('#')[1]).parent().parent().find(".body").first().html();
 
 					body.html(content);
 				}
