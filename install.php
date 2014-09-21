b3a2c62cbd1eca4a01dbddec627d53ea60e0b8b5
czaks <marcin@6irc.net>
2014-09-21 05:58:49 +0200

    tag for 4.9.92

--- install.php
+++ install.php
@@ -1,7 +1,7 @@
 <?php
 
 // Installation/upgrade file	
-define('VERSION', '4.9.91');
+define('VERSION', '4.9.92');
 
 require 'inc/functions.php';
 
@@ -546,6 +546,7 @@ if (file_exists($config['has_installed'])) {
 				query(sprintf('REPAIR TABLE ``posts_%s``', $board['uri']));
 			}
 		case '4.9.90':
+		case '4.9.91':
 		case false:
 			// TODO: enhance Tinyboard -> vichan upgrade path.
 			query("CREATE TABLE IF NOT EXISTS ``search_queries`` (  `ip` varchar(39) NOT NULL,  `time` int(11) NOT NULL,  `query` text NOT NULL) ENGINE=MyISAM DEFAULT CHARSET=utf8;") or error(db_error());
