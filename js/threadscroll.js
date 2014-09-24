b9886dc474e5d3fcbf879bd6237bae739c63a9f1
anonfagola <anonfagola@users.noreply.github.com>
2014-09-23 19:31:06 -0700

    Update threadscroll.js
    Made compatible for use with additional_javascripts.

--- js/threadscroll.js
+++ js/threadscroll.js
@@ -1,35 +1,37 @@
-var hoverElem = null;
-
-$(document).mouseover(function(e){
-	var x = e.clientX, y = e.clientY,
-		elementOnMouseOver = document.elementFromPoint(x, y);
-		hoverElem = $(elementOnMouseOver);
-});
-
-$(document).keydown(function(e){
-	//Up arrow
-	if(e.which == 38){
-		var ele = hoverElem;
-		var par = $(ele).parents('div[id^="thread_"]');
-		
-		if(par.length == 1){
-			if(par.prev().attr("id") != null){
-				if(par.prev().attr("id").match("^thread")){
-					window.location.href = window.location.protocol+"//"+window.location.host+window.location.pathname+"#"+par.prev().attr("id");
+if(active_page == "index" || active_page == "ukko"){
+	var hoverElem = null;
+	
+	$(document).mouseover(function(e){
+		var x = e.clientX, y = e.clientY,
+			elementOnMouseOver = document.elementFromPoint(x, y);
+			hoverElem = $(elementOnMouseOver);
+	});
+	
+	$(document).keydown(function(e){
+		//Up arrow
+		if(e.which == 38){
+			var ele = hoverElem;
+			var par = $(ele).parents('div[id^="thread_"]');
+			
+			if(par.length == 1){
+				if(par.prev().attr("id") != null){
+					if(par.prev().attr("id").match("^thread")){
+						window.location.href = window.location.protocol+"//"+window.location.host+window.location.pathname+"#"+par.prev().attr("id");
+					}
+				}
+			}
+		//Down arrow
+		}else if(e.which == 40){
+			var ele = hoverElem;
+			var par = $(ele).parents('div[id^="thread_"]');
+			
+			if(par.length == 1){
+				if(par.next().attr("id") != null){
+					if(par.next().attr("id").match("^thread")){
+						window.location.href = window.location.protocol+"//"+window.location.host+window.location.pathname+"#"+par.next().attr("id");
+					}
 				}
 			}
 		}
-	//Down arrow
-	}else if(e.which == 40){
-		var ele = hoverElem;
-		var par = $(ele).parents('div[id^="thread_"]');
-		
-		if(par.length == 1){
-			if(par.next().attr("id") != null){
-				if(par.next().attr("id").match("^thread")){
-					window.location.href = window.location.protocol+"//"+window.location.host+window.location.pathname+"#"+par.next().attr("id");
-				}
-			}
-		}
-	}
-});
+	});
+}
