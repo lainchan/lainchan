22f4543e6fb40f4f76143fdf8ef6cf5e0487c38a
Bui <bui@bui.pm>
2014-09-28 08:58:39 +0900

    remove vertical padding on coloured IDs

--- js/id_colors.js
+++ js/id_colors.js
@@ -28,7 +28,7 @@ if (active_page == 'thread' || active_page == 'index') {
 			
 			$(el).css({
 				"background-color": "rgb("+rgb[0]+", "+rgb[1]+", "+rgb[2]+")",
-				"padding": "3px 5px",
+				"padding": "0px 5px",
 				"border-radius": "8px",
 				"color": "white"
 			});
