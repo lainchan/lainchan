3c2529e157c4f51e59e107016c4c06d3026d43d6
kaf <admin@cable6.net>
2014-10-05 12:03:30 +0000

    [bugfix]$config['error']['genwebmerror'] wasn't set in config.php

--- inc/config.php
+++ inc/config.php
@@ -1046,10 +1046,11 @@
 	$config['error']['unknownext']		= _('Unknown file extension.');
 	$config['error']['filesize']		= _('Maximum file size: %maxsz% bytes<br>Your file\'s size: %filesz% bytes');
 	$config['error']['maxsize']		= _('The file was too big.');
-	$config['error']['webmerror'] = _('There was a problem processing your webm.');
-	$config['error']['invalidwebm'] = _('Invalid webm uploaded.');
-	$config['error']['webmhasaudio'] = _('The uploaded webm contains an audio or another type of additional stream.');
-	$config['error']['webmtoolong'] = _('The uploaded webm is longer than ' . $config['webm']['max_length'] . ' seconds.');
+	$config['error']['genwebmerror']	= _('There was a problem processing your webm.');
+	$config['error']['webmerror'] 		= _('There was a problem processing your webm.');//Is this error used anywhere ?
+	$config['error']['invalidwebm'] 	= _('Invalid webm uploaded.');
+	$config['error']['webmhasaudio'] 	= _('The uploaded webm contains an audio or another type of additional stream.');
+	$config['error']['webmtoolong'] 	= _('The uploaded webm is longer than ' . $config['webm']['max_length'] . ' seconds.');
 	$config['error']['fileexists']		= _('That file <a href="%s">already exists</a>!');
 	$config['error']['fileexistsinthread']	= _('That file <a href="%s">already exists</a> in this thread!');
 	$config['error']['delete_too_soon']	= _('You\'ll have to wait another %s before deleting that.');
