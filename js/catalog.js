4f96263e3b72124df7b152e5dd2b513683fda818
8chan <admin@8chan.co>
2014-10-21 21:06:34 -0700

    Upgrade jQuery mixitup, preserve settings, no animations

--- js/catalog.js
+++ js/catalog.js
@@ -1,8 +1,17 @@
 if (active_page == 'catalog') $(function(){
 
+	if (localStorage.catalog !== undefined) {
+		var catalog = JSON.parse(localStorage.catalog);
+	} else {
+		var catalog = {};
+		localStorage.catalog = JSON.stringify(catalog);
+	}
+
 	$("#sort_by").change(function(){
 		var value = this.value;
-		$("#sort-"+value).trigger("click");
+		$('#Grid').mixItUp('sort', value);
+		catalog.sort_by = value;
+		localStorage.catalog = JSON.stringify(catalog);
 	});
 
 	$("#image_size").change(function(){
@@ -11,9 +20,21 @@ if (active_page == 'catalog') $(function(){
 		$(".grid-li").removeClass("grid-size-small");
 		$(".grid-li").removeClass("grid-size-large");
 		$(".grid-li").addClass("grid-size-"+value);
+		catalog.image_size = value;
+		localStorage.catalog = JSON.stringify(catalog);
 	});
 
-	$('#Grid').mixitup({
+	$('#Grid').mixItUp({
+		animation: {
+			enable: false
+		},
 	});
+
+	if (catalog.sort_by !== undefined) {
+		$('#sort_by').val(catalog.sort_by).trigger('change');
+	}
+	if (catalog.image_size !== undefined) {
+		$('#image_size').val(catalog.image_size).trigger('change');
+	}
 
 });
