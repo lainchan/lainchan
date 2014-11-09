4e39262223eb255904fcd86532058e810e244b79
Forkless <REDACTED@gmail.com>
2014-11-08 22:17:33 -0600

    Moved the Options tab stuff to be setup after the document is ready.

--- js/comment-toolbar.js
+++ js/comment-toolbar.js
@@ -251,74 +251,74 @@ if (active_page == 'thread' || active_page == 'index') {
 		// setup default rules for customizing
 		if (!localStorage.formatText_rules) localStorage.formatText_rules = JSON.stringify(self.rules);
 		
-		// Add settings to Options panel general tab
-		if (window.Options && Options.get_tab('general')) {
-			var s1 = '#formatText_keybinds>input', s2 = '#formatText_toolbar>input', e = 'change';
-			Options.extend_tab('general', '\
-				<fieldset>\
-					<legend>Formatting Options</legend>\
-					<label id="formatText_keybinds"><input type="checkbox">' + _('Enable formatting keybinds') + '</label>\
-					<label id="formatText_toolbar"><input type="checkbox">' + _('Show formatting toolbar') + '</label>\
-				</fieldset>\
-			');
-		} else {
-			var s1 = '#formatText_keybinds', s2 = '#formatText_toolbar', e = 'click';
-			$('hr:first').before('<div id="formatText_keybinds" style="text-align:right"><a class="unimportant" href="javascript:void(0)">'+ _('Enable formatting keybinds') +'</a></div>');
-			$('hr:first').before('<div id="formatText_toolbar" style="text-align:right"><a class="unimportant" href="javascript:void(0)">'+ _('Show formatting toolbar') +'</a></div>');
-		}
-		
-		// add the tab for customizing the format settings
-		if (window.Options && !Options.get_tab('formatting')) {
-			Options.add_tab('formatting', 'angle-right', 'Customize Formatting');
-			Options.extend_tab('formatting', '\
-			<style>\
-				.format_option{\
-					margin-right:5px;\
-					overflow:initial;\
-					font-size:15px;\
-				}\
-				.format_option[type="text"]{\
-					text-align:center;\
-					padding-bottom: 2px;\
-					padding-top: 2px;\
-				}\
-				.format_option:last-child{\
-					margin-right:0;\
-				}\
-				fieldset{\
-					margin-top:5px;\
-				}\
-			</style>\
-			');
-			
-			// Data control row
-			Options.extend_tab('formatting', '\
-			<button onclick="formatText.add_rule();">Add Rule</button>\
-			<button onclick="formatText.save_rules();">Save Rules</button>\
-			<button onclick="formatText.reset_rules(false);">Revert</button>\
-			<button onclick="formatText.reset_rules(true);">Reset to Default</button>\
-			');
-			
-			// Descriptor row
-			Options.extend_tab('formatting', '\
-				<span class="format_option" style="margin-left:25px;">Name</span>\
-				<span class="format_option" style="margin-left:45px;" title="Multi-line: Allow formatted area to contain linebreaks.">ML</span>\
-				<span class="format_option" style="margin-left:0px;" title="Exclusive-line: Require formatted area to start after and end before a linebreak.">EL</span>\
-				<span class="format_option" style="margin-left:25px;" title="Text injected at the start of a format area.">Prefix</span>\
-				<span class="format_option" style="margin-left:60px;" title="Text injected at the end of a format area.">Suffix</span>\
-				<span class="format_option" style="margin-left:40px;" title="Optional keybind value to allow keyboard shortcut access.">Key</span>\
-			');
-			
-			// Rule rows
-			var rules = JSON.parse(localStorage.formatText_rules);
-			for (var index in rules){
-				if (!rules.hasOwnProperty(index)) continue;
-				self.add_rule(rules[index], index);
-			}
-		}
-		
 		// setup code to be ran when page is ready (work around for main.js compilation).
 		$(document).ready(function(){
+			// Add settings to Options panel general tab
+			if (window.Options && Options.get_tab('general')) {
+				var s1 = '#formatText_keybinds>input', s2 = '#formatText_toolbar>input', e = 'change';
+				Options.extend_tab('general', '\
+					<fieldset>\
+						<legend>Formatting Options</legend>\
+						<label id="formatText_keybinds"><input type="checkbox">' + _('Enable formatting keybinds') + '</label>\
+						<label id="formatText_toolbar"><input type="checkbox">' + _('Show formatting toolbar') + '</label>\
+					</fieldset>\
+				');
+			} else {
+				var s1 = '#formatText_keybinds', s2 = '#formatText_toolbar', e = 'click';
+				$('hr:first').before('<div id="formatText_keybinds" style="text-align:right"><a class="unimportant" href="javascript:void(0)">'+ _('Enable formatting keybinds') +'</a></div>');
+				$('hr:first').before('<div id="formatText_toolbar" style="text-align:right"><a class="unimportant" href="javascript:void(0)">'+ _('Show formatting toolbar') +'</a></div>');
+			}
+			
+			// add the tab for customizing the format settings
+			if (window.Options && !Options.get_tab('formatting')) {
+				Options.add_tab('formatting', 'angle-right', 'Customize Formatting');
+				Options.extend_tab('formatting', '\
+				<style>\
+					.format_option{\
+						margin-right:5px;\
+						overflow:initial;\
+						font-size:15px;\
+					}\
+					.format_option[type="text"]{\
+						text-align:center;\
+						padding-bottom: 2px;\
+						padding-top: 2px;\
+					}\
+					.format_option:last-child{\
+						margin-right:0;\
+					}\
+					fieldset{\
+						margin-top:5px;\
+					}\
+				</style>\
+				');
+				
+				// Data control row
+				Options.extend_tab('formatting', '\
+				<button onclick="formatText.add_rule();">Add Rule</button>\
+				<button onclick="formatText.save_rules();">Save Rules</button>\
+				<button onclick="formatText.reset_rules(false);">Revert</button>\
+				<button onclick="formatText.reset_rules(true);">Reset to Default</button>\
+				');
+				
+				// Descriptor row
+				Options.extend_tab('formatting', '\
+					<span class="format_option" style="margin-left:25px;">Name</span>\
+					<span class="format_option" style="margin-left:45px;" title="Multi-line: Allow formatted area to contain linebreaks.">ML</span>\
+					<span class="format_option" style="margin-left:0px;" title="Exclusive-line: Require formatted area to start after and end before a linebreak.">EL</span>\
+					<span class="format_option" style="margin-left:25px;" title="Text injected at the start of a format area.">Prefix</span>\
+					<span class="format_option" style="margin-left:60px;" title="Text injected at the end of a format area.">Suffix</span>\
+					<span class="format_option" style="margin-left:40px;" title="Optional keybind value to allow keyboard shortcut access.">Key</span>\
+				');
+				
+				// Rule rows
+				var rules = JSON.parse(localStorage.formatText_rules);
+				for (var index in rules){
+					if (!rules.hasOwnProperty(index)) continue;
+					self.add_rule(rules[index], index);
+				}
+			}
+		
 			// setting for enabling formatting keybinds
 			$(s1).on(e, function(e) {
 				console.log('Keybind');
