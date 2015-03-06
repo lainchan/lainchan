a9b29c723236d46c2e767c8696a536abab26a900
marktaiwan <mark.taiwan@gmail.com>
2015-03-06 20:04:35 +0800

    Bugfix: convert it to string

--- js/post-filter.js
+++ js/post-filter.js
@@ -811,7 +811,7 @@ if (active_page === 'thread' || active_page === 'index' || active_page === 'cata
 			if (active_page == 'catalog') {
 				$(document).on('click', '.mix', function(e) {
 					if (e.shiftKey) {
-						var threadId = $(this).data('id');
+						var threadId = $(this).data('id').toString();
 						var postId = threadId;
 						blacklist.add.post(pageData.boardId, threadId, postId, false);
 					}
