9fa320838b19506d2bfd45f995298e75766530d9
czaks <marcin@6irc.net>
2015-03-23 01:09:16 +0100

    wpaint has new dependencies – important!

--- js/wpaint.js
+++ js/wpaint.js
@@ -13,12 +13,7 @@
  *   $config['additional_javascript'][] = 'js/jquery.min.js';
  *   $config['additional_javascript'][] = 'js/jquery-ui.custom.min.js';
  *   $config['additional_javascript'][] = 'js/ajax.js';
- *   $config['additional_javascript'][] = 'js/wPaint/lib/wColorPicker.min.js';
- *   $config['additional_javascript'][] = 'js/wPaint/wPaint.min.js';
- *   $config['additional_javascript'][] = 'js/wPaint/plugins/main/wPaint.menu.main.min.js';
- *   $config['additional_javascript'][] = 'js/wPaint/plugins/text/wPaint.menu.text.min.js';
- *   $config['additional_javascript'][] = 'js/wPaint/plugins/shapes/wPaint.menu.main.shapes.min.js';
- *   $config['additional_javascript'][] = 'js/wPaint/plugins/file/wPaint.menu.main.file.min.js';
+ *   $config['additional_javascript'][] = 'js/wPaint/8ch.js';
  *   $config['additional_javascript'][] = 'js/wpaint.js';
  *   $config['additional_javascript'][] = 'js/upload-selection.js';
  *
