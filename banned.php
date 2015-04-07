0658a2b19abbd34f0675f1940aa680eb3dee7139
czaks <marcin@6irc.net>
2015-04-07 02:29:38 +0200

    fix banned.php; thanks stigma

--- banned.php
+++ banned.php
@@ -1,5 +1,6 @@
 <?php
  require_once 'inc/functions.php';
+ require_once 'inc/bans.php';
  checkBan();
  print "<!doctype html><html><head><meta charset='utf-8'><title>"._("Banned?")."</title></head><body>";
  print "<h1>"._("You are not banned.")."</h1>";
