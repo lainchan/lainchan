8cc3bd12e3082166d7522f9a487125baf5d7fefa
Simon McFarlane <simon@piston.pw>
2015-05-27 04:59:05 +0200

    Add background scripts

---
+++ bg.php
@@ -0,0 +1,20 @@
+<?php
+//renaming to bg.php
+$dir = "walls/";
+$files = scandir($dir);
+$images = array_diff($files, array('.', '..'));
+$name = $images[array_rand($images)];
+// open the file in a binary mode
+$fp = fopen($dir . $name, 'rb');
+
+// send the right headers
+header('Cache-Control: no-cache, no-store, must-revalidate'); // HTTP 1.1
+header('Pragma: no-cache'); // HTTP 1.0
+header('Expires: 0'); // Proxies
+header('Content-Type: ' . $fp['type']);
+header('Content-Length: ' . $fp['bytes']);
+
+// dump the picture and stop the script
+fpassthru($fp);
+exit;
+?>
