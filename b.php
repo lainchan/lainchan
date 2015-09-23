9679f970cbfcd839f61466533788eb6afe033ca1
Kashire <ashtonthecoder@gmail.com>
2015-09-22 19:25:17 -0500

    Update b.php
    added path barrucadu's suggestion, not sure why I didn't see it for like 3 days.

--- b.php
+++ b.php
@@ -12,7 +12,7 @@ $img_extension = pathinfo($name, PATHINFO_EXTENSION);
 header('Cache-Control: no-cache, no-store, must-revalidate'); // HTTP 1.1
 header('Pragma: no-cache'); // HTTP 1.0
 header('Expires: 0'); // Proxies
-header("Content-type: " . $img_extension);
+header("Content-type: image/" . $img_extension);
 header("Content-Disposition: inline; filename=" . $name);
 
 // readfile displays the image, passthru seems to spits stream.
