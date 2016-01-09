9e1dda03483846ad7ccb30feefdbf18240374b27
Simon McFarlane <simon@piston.pw>
2016-01-09 01:31:28 +0000

    Update piwik JS to match config

--- js/piwik.js
+++ js/piwik.js
@@ -1,24 +1,12 @@
-/*
- * piwik.js
- *analytics code for lainchan
- * Usage:
- *   $config['additional_javascript'][] = 'js/piwik.js';
- *
- */
 
-
-<!-- Piwik -->
   var _paq = _paq || [];
-  _paq.push(["setDocumentTitle", document.domain + "/" + document.title]);
-  _paq.push(["setCookieDomain", "*.lainchan.org"]);
   _paq.push(['trackPageView']);
   _paq.push(['enableLinkTracking']);
   (function() {
-    var u="//lainchan.org/piwik/";
+    var u="//lainchan.org/analytics/piwik/";
     _paq.push(['setTrackerUrl', u+'piwik.php']);
-    _paq.push(['setSiteId', 3]);
+    _paq.push(['setSiteId', 1]);
     var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
     g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
   })();
-<!-- End Piwik Code -->
 
