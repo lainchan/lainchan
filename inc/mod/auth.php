cd01191072abc5dfe07245c768a39e3977d612bd
czaks <marcin@6irc.net>
2016-05-05 11:45:29 +0200

    those parts are extraneous

--- inc/mod/auth.php
+++ inc/mod/auth.php
@@ -212,17 +212,4 @@ function check_login($prompt = false) {
 			'boards' => explode(',', $user['boards'])
 		);
 	}
-
-	if ($config['debug'])
-		$parse_start_time = microtime(true);
-
-	// Fix for magic quotes
-	if (get_magic_quotes_gpc()) {
-		function strip_array($var) {
-			return is_array($var) ? array_map('strip_array', $var) : stripslashes($var);
-		}
-		
-		$_GET = strip_array($_GET);
-		$_POST = strip_array($_POST);
-	}
 }
