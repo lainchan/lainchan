abe4bdd6aeb28566f3bca4385f110f6e7bf759d8
czaks <marcin@6irc.net>
2016-05-05 10:52:58 +0200

    fixup

--- inc/api.php
+++ inc/api.php
@@ -92,7 +92,7 @@ class Api {
 		$dotPos = strrpos($file->file, '.');
 		$apiPost['ext'] = substr($file->file, $dotPos);
 		$apiPost['tim'] = substr($file->file, 0, $dotPos);
-		if (isset ($file->hash) && $post->filehash) {
+		if (isset ($file->hash) && $file->hash) {
 			$apiPost['md5'] = base64_encode(hex2bin($file->hash));
 		}
 		else if (isset ($post->filehash) && $post->filehash) {
