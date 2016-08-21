b6b2049a32199e3865d73ed437eb3a3af2bb66fa
Milosz Kwiatkowski <m.kiwi2000@gmail.com>
2016-08-21 20:31:14 +0100

    fix JQ document ready trigger

--- js/rules-popup.js
+++ js/rules-popup.js
@@ -11,32 +11,32 @@
  *   $config['additional_javascript'][] = 'js/rules-popup.js';
  *
  */
+    if (typeof localStorage.rulesAccepted === "undefined") {
 
-if (typeof localStorage.rulesAccepted === "undefined") {
+        // generate a 7-character long random string
+        captcha = Math.random().toString(36).substring(2, 9)
 
-    // generate a 7-character long random string
-    captcha = Math.random().toString(36).substring(2, 9)
+        $("<div id='rules-popup'>")
+            .prependTo("body");
 
-    $("<div id='rules-popup'>")
-        .prependTo("body");
+        $("#rules-popup")
+            .append("<div class='rules-popup-top'>lainchan rule agreement</div>")
+            .append("<div class='rules-popup-content-wrapper'></div>")
+            .append("<div class='rules-popup-bottom'></div>");
 
-    $("#rules-popup")
-        .append("<div class='rules-popup-top'>lainchan rule agreement</div>")
-        .append("<div class='rules-popup-content-wrapper'></div>")
-        .append("<div class='rules-popup-bottom'></div>");
+        $(".rules-popup-content-wrapper")
+            .append("<div id='rules-popup-content'></div>");
+        $("#rules-popup-content")
+            .load("/templates/rules.html");
 
-    $(".rules-popup-content-wrapper")
-        .append("<div id='rules-popup-content'></div>");
-    $("#rules-popup-content")
-        .load("/templates/rules.html");
-
-    $(".rules-popup-bottom")
-        .append("<div class='rules-popup-bottom-instructions'>If you accept the rules, retype the captcha and press ACCEPT.</div>")
-        .append("<div class='rules-popup-captcha-wrapper'></div>");
-    $(".rules-popup-captcha-wrapper")
-        .append("<div class='rules-popup-captcha'>" + captcha + "</div>")
-        .append("<form class='rules-popup-form' onsubmit=\"if ($('#captcha').val() == '" + captcha + "') { localStorage.rulesAccepted = 1; $('#rules-popup').remove(); } return false;\"></form>");
-    $(".rules-popup-form")
-        .append("<input class='rules-popup-form-input' type='text' id='captcha' />")
-        .append("<input type='submit' value='ACCEPT' />");
-}
+        $(".rules-popup-bottom")
+            .append("<div class='rules-popup-bottom-instructions'>If you accept the rules, retype the captcha and press ACCEPT.</div>")
+            .append("<div class='rules-popup-captcha-wrapper'></div>");
+        $(".rules-popup-captcha-wrapper")
+            .append("<div class='rules-popup-captcha'>" + captcha + "</div>")
+            .append("<form class='rules-popup-form' onsubmit=\"if ($('#captcha').val() == '" + captcha + "') { localStorage.rulesAccepted = 1; $('#rules-popup').remove(); } return false;\"></form>");
+        $(".rules-popup-form")
+            .append("<input class='rules-popup-form-input' type='text' id='captcha' />")
+            .append("<input type='submit' value='ACCEPT' />");
+    }
+});
