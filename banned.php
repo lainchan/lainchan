8525aaabfa0dfd2b18cbd531339a5d8f94d54f09
JamesJDillon <J.Dillon@2014.ljmu.ac.uk>
2016-08-28 23:33:23 +0100

    Added a better 'not banned' response if a user navigates to banned.php whilst not being banned.

--- banned.php
+++ banned.php
@@ -1,8 +1,18 @@
 <?php
- require_once 'inc/functions.php';
- require_once 'inc/bans.php';
- checkBan();
- print "<!doctype html><html><head><meta charset='utf-8'><title>"._("Banned?")."</title></head><body>";
- print "<h1>"._("You are not banned.")."</h1>";
- print "</body></html>";
+  require_once 'inc/functions.php';
+  require_once 'inc/bans.php';
+  checkBan();
+  print "<!doctype html><html><head><meta charset='utf-8'><title>"._("Banned?")."</title></head><body>";
+
+  //If the user is not banned, show the "not banned" page.
+  die(
+    Element('page.html', array(
+      'title' => _('Not banned!'),
+      'config' => $config,
+      'nojavascript' => true,
+      'body' => Element('notbanned.html', array()
+    ))
+  )); 
+
+  print "</body></html>";
 ?>
