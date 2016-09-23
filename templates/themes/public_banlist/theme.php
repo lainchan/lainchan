8b5688e12a3687f86c8c40d36a1327445cfb8143
Benjamin Southall <applemansigloo@gmail.com>
2016-09-23 22:33:20 +0900

    Add porn to public banlist theme

--- templates/themes/public_banlist/theme.php
+++ templates/themes/public_banlist/theme.php
@@ -25,7 +25,7 @@
 
 		public static function gen_json($settings) {
 			ob_start();
-			Bans::stream_json(false, true, true, array(), array("/\bcp\b/i"));
+			Bans::stream_json(false, true, true, array(), array("/\bcp\b/i", "/porn/i"));
 			$out = ob_get_contents();
 			ob_end_clean();
 			return $out;
