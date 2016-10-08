fa01d816b3b2d9dfb4f6b37435d1a6fd3cbf76f7
Benjamin Southall <applemansigloo@gmail.com>
2016-10-08 15:29:06 +0900

    Add stream theme / extension to Lainchan

---
+++ templates/themes/stream/info.php
@@ -0,0 +1,74 @@
+<?php
+	$theme = Array();
+	
+	// Theme name
+	$theme['name'] = 'Stream';
+	// Description (you can use Tinyboard markup here)
+	$theme['description'] = 'Stream page';
+	$theme['version'] = 'v0.9.1';
+	
+	// Theme configuration	
+	$theme['config'] = Array();
+	
+	$theme['config'][] = Array(
+		'title' => 'Site title',
+		'name' => 'title',
+		'type' => 'text'
+	);
+	
+	$theme['config'][] = Array(
+		'title' => 'Slogan',
+		'name' => 'subtitle',
+		'type' => 'text',
+		'comment' => '(optional)'
+	);
+	
+	$theme['config'][] = Array(
+		'title' => 'File',
+		'name' => 'file',
+		'type' => 'text',
+		'default' => 'stream.html',
+		'comment' => '(eg. "stream.html")'
+	);
+    $theme['config'][] =  Array(
+              'title'   => 'OGV stream URL',
+              'name'    => 'ogvurl',
+              'type'    => 'text',
+              'default' => 'https://lainchan.org/radio_assets/lainstream.ogg')
+          );
+    $theme['config'][] =  Array(
+              'title'   => 'RTMP stream URL',
+              'name'    => 'rtmpurl',
+              'type'    => 'text',
+              'default' => 'rtmp://lainchan.org/live/&stream')
+          );
+
+    $theme['config'][] =  Array(
+              'title'   => 'OGV Status URL',
+              'name'    => 'ogvstatus',
+              'type'    => 'text',
+              'default' => '/radio_assets/status.xsl')
+          );
+    
+    $theme['config'][] =  Array(
+              'title'   => 'RTMP Status URL',
+              'name'    => 'rtmpstatus',
+              'type'    => 'text',
+              'default' => '/live/status?app=live&name=stream')
+          );
+    $theme['config'][] =  Array(
+              'title'   => 'RTMP Viewers URL',
+              'name'    => 'rtmpviewers',
+              'type'    => 'text',
+              'default' => '/live/subs?app=live&name=stream')
+          );
+	
+	
+	// Unique function name for building everything
+	$theme['build_function'] = 'stream_build';
+	$theme['install_callback'] = 'stream_install';
+	if (!function_exists('stream_install')) {
+		function stream_install($settings) {
+		}
+	}
+
---
+++ templates/themes/stream/theme.php
@@ -0,0 +1,34 @@
+<?php
+	require 'info.php';
+	
+	function stream_build($action, $settings, $board) {
+		// Possible values for $action:
+		//	- all (rebuild everything, initialization)
+		//	- boards (board list changed)
+		
+		Stream::build($action, $settings);
+	}
+	
+	// Wrap functions in a class so they don't interfere with normal Tinyboard operations
+	class Stream {
+		public static function build($action, $settings) {
+			global $config;
+			
+			if ($action == 'all' )
+				file_write($config['dir']['home'] . $settings['file'], Stream::homepage($settings));
+		}
+		
+		// Build news page
+		public static function homepage($settings) {
+			global $config;
+			
+			
+			return Element('themes/stream/stream.html', Array(
+				'settings' => $settings,
+				'config' => $config,
+				'boardlist' => createBoardlist(),
+			));
+		}
+	};
+	
+?>
