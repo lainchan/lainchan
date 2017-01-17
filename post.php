d83376f155fb493d6e2303cae200f202161965b7
Benjamin Southall <applemansigloo@gmail.com>
2017-01-17 22:55:46 +0900

    Add min_body configuration option for post too short check

--- post.php
+++ post.php
@@ -730,6 +730,8 @@ if (isset($_POST['delete'])) {
 			error(sprintf($config['error']['toolong'], 'subject'));
 		if (!$mod && mb_strlen($post['body']) > $config['max_body'])
 			error($config['error']['toolong_body']);
+		if (!$mod && mb_strlen($post['body']) > 0 && mb_strlen($post['body']) < $config['min_body'])
+			error($config['error']['tooshort_body']);
 		if (mb_strlen($post['password']) > 20)
 			error(sprintf($config['error']['toolong'], 'password'));
 	}
