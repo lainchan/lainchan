a69bc13ba21482b9376cf6071425a64eed31c746
Benjamin Southall <applemansigloo@gmail.com>
2017-03-03 15:34:45 +0900

    Change code hightlighting to highlight.js from google-code prettify.js

--- js/hilight.js
+++ js/hilight.js
@@ -1,11 +1,7 @@
 function styleCode() {
-    $('pre').each(function() {
-        if (!$(this).hasClass('prettyprint')) {
-            $(this).addClass('prettyprint');
-        }
-    });
-
-    prettyPrint();
+hljs.configure({'useBR' : true, 'languages': 
+[ 'nohighlight',,'ada','armasm','arm','avrasm','actionscript','as','apache','apacheconf','applescript','osascript','asciidoc','adoc','aspectj','autohotkey','autoit','awk','mawk','nawk','gawk','axapta','bash','sh','zsh','basic','bnf','brainfuck','bf','cs','csharp','cpp','c','cc','h','c++','h++','hpp','cal','cos','cls','cmake','cmake.in','coq','csp','css','capnproto','capnp','clojure','clj','coffeescript','coffee','cson','iced','crystal','cr','d','dns','zone','bind','dos','bat','cmd','dart','delphi','dpr','dfm','pas','pascal','freepascal','lazarus','lpr','lfm','diff','patch','django','jinja','dockerfile','docker','dsconfig','dust','dst','ebnf','elixir','elm','erlang','erl','fsharp','fs','fix','fortran','f90','f95','gcode','nc','gams','gms','gauss','gss','gherkin','go','golang','golo','gololang','gradle','groovy','xml','html','xhtml','rss','atom','xjb','xsd','xsl','plist','http','https','haml','handlebars','hbs','html.hbs','html.handlebars','haskell','hs','haxe','hx','inform7','i7','json','java','jsp','javascript','js','jsx','lasso','ls','lassoscript','less','ldif','lisp','livecodeserver','lua','makefile','mk','mak','markdown','md','mkdown','mkd','mathematica','mma','matlab','maxima','mel','mercury','mizar','mojolicious','moonscript','moon','nsis','nginx','nginxconf','nimrod','nim','nix','ocaml','ml','objectivec','mm','objc','obj-c','glsl','openscad','scad','ruleslanguage','oxygene','pf','pf.conf','php','php3','php4','php5','php6','parser3','perl','pl','pm','pony','powershell','ps','prolog','protobuf','puppet','pp','python','py','gyp','profile','k','kdb','qml','r','rib','rsl','graph','instances','ruby','rb','gemspec','podspec','thor','irb','rust','rs','scss','sql','p21','step','stp','scala','scheme','scilab','sci','smali','smalltalk','st','stan','stylus','styl','subunit','swift','tap','tcl','tk','tex','thrift','tp','twig','craftcms','typescript','ts','vbnet','vb','vbscript','vbs','vhdl','vala','verilog','v','vim','x86asm','xl','tao','xpath','xq','zephir','zep']   });
+hljs.initHighlightingOnLoad();
 }
 
 $(function() {styleCode();});
